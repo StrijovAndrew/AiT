fb602b172e17884cd7ffab6f0516cd207d93629f
StrijovAndrew <andreyy0strijov@gmail.com>
2022-03-30 09:35:03 +0500

    first

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Timus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N;
+            int M;
+
+            string s = Console.ReadLine();
+            string[] nums = s.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            N = Convert.ToInt32(nums[0]);
+            M = Convert.ToInt32(nums[1]);
+            int output = N * M;
+            Console.WriteLine(output);
+        }
+    }
+}
